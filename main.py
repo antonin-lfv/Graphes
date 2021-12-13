79f3ce20e07d86ac5213d0f555d7e53f12531555
Antonin <antoninlefevre45@icloud.com>
2021-12-13 20:05:16 +0100

    init

--- main.py
+++ main.py
@@ -100,9 +100,9 @@ elif choix == "Dijkstra graphe 2D depuis csv":
     dijkstra_edges, dijkstra_nodes = set_weights(edge_csv_2d, node_csv_2d)
     G = graphe(dijkstra_edges)
     sommet_depart = st.sidebar.slider(label="sommet de départ", min_value=min(G.keys()),
-                                              max_value=max(G.keys()), value=min(G.keys()), step=1)
+                                              max_value=max(G.keys()), step=1)
     sommet_arrivee = st.sidebar.slider(
-        label="sommet d'arrivée", min_value=min(G.keys()), max_value=max(G.keys()), value=max(G.keys()), step=1)
+        label="sommet d'arrivée", min_value=min(G.keys()), max_value=max(G.keys()), step=1)
     st.write("##")
     if st.sidebar.button("Générer le graphe"):
         dijkstra_resultat = make_path(parent=dijkstra(G=G, start=sommet_depart, goal=sommet_arrivee),
@@ -125,9 +125,9 @@ elif choix == "Dijkstra graphe random 2D ":
     dijkstra_edges, dijkstra_nodes = set_weights(random_edges_2D, random_node_2D)
     G = graphe(dijkstra_edges)
     sommet_depart = st.sidebar.slider(label="sommet de départ", min_value=min(G.keys()),
-                                              max_value=max(G.keys()), value=min(G.keys()), step=1)
+                                              max_value=max(G.keys()), step=1)
     sommet_arrivee = st.sidebar.slider(
-        label="sommet d'arrivée", min_value=min(G.keys()), max_value=max(G.keys()), value=max(G.keys()), step=1)
+        label="sommet d'arrivée", min_value=min(G.keys()), max_value=max(G.keys()), step=1)
     st.write("##")
     if st.sidebar.button("Générer le graphe"):
         dijkstra_resultat = make_path(parent=dijkstra(G=G, start=sommet_depart, goal=sommet_arrivee),
